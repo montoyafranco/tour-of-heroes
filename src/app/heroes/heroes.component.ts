1347a3da2c985aa18d756e7576c652d43fad6bdb
montoyafranco <francomontoya9810@gmail.com>
2022-09-05 01:07:49 -0300

    creating messagges and async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,20 +12,22 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
-  
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
-
-  ngOnInit(): void {
-  this.getHeroes();
-}
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
